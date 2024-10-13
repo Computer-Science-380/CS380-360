0ce2a4c01c432674b9a93ec6137f562507e4145b
GustavoHu <166774175+GustavoHu@users.noreply.github.com>
2024-10-12 21:18:54 -0600

    assingment

--- src/App.js
+++ src/App.js
@@ -4,8 +4,6 @@
   import Navbar from './components/Navbar';
   import MainMenu from './components/MainMenu';
   import MatrixEffect from './components/MatrixEffect'; // Importa el efecto Matrix
-  import BaconCipher from './components/BaconCipher'; // Importa el componente BaconCipher
-  import MorseCipher from './components/MorseCipher'; // Importa el componente MorseCipher
   import './App.css';
 
   function App() {
@@ -16,8 +14,8 @@
           <Navbar />
           <Routes>
             <Route path="/" element={<MainMenu />} />
-            <Route path="/bacon" element={<BaconCipher />} /> {/* Ruta para BaconCipher */}
-            <Route path="/morse" element={<MorseCipher />} /> {/* Ruta para MorseCipher */}
+            <Route path="/bacon" element /> {/* Ruta para BaconCipher */}
+            <Route path="/morse" element /> {/* Ruta para MorseCipher */}
             <Route path="/about" element={<div>About Page</div>} />
           </Routes>
         </div>
