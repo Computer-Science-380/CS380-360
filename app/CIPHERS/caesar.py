5e2f4dac2bae632f1b92300681a814a47edc839f
FunpopSDM <legoguy181@gmail.com>
2024-10-14 15:27:58 -0600

    changed super class and added test_caesar.py to the Test folder

---
+++ app/CIPHERS/caesar.py
@@ -0,0 +1,15 @@
+from cipher_SuperClass import Cipher
+
+class caesar(Cipher):
+    
+    def encrypt(self, plainTxt, key: int):
+        codeTxt: str = ""
+
+        
+
+        return codeTxt
+
+
+
+#Manual Test Caesar Cipher  
+caesar.encrypt(caesar,"Hello World!", 3)
